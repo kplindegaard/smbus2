46badb03daedecd9536dc0836fbb42efb73991b6
Thijs Triemstra <info@collab.nl>
2017-12-27 19:08:51 +0100

    add more classifiers

--- setup.py
+++ setup.py
@@ -21,7 +21,11 @@ setup(
         "Development Status :: 4 - Beta",
         "Topic :: Utilities",
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3"
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6"
     ],
 )
